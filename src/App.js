2115f4d41a9f4c320c4244baa7a6c3e1861386b8
agalaev <agalaev@kameleoon.com>
2022-07-02 01:17:00 +0300

    Pulemet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from './components/Header/header'
+import Nav from "./components/Nav/navbar";
+import Profile from "./components/Profile/profile";
+import {Routes,  Route} from "react-router-dom";
+import Musics from "./components/Musics/musics";
+import Settings from "./components/Settings/settings";
+import DialogsContainer from "./components/Dialogs/dialogsContainer";
+import UsersContainer from "./components/Users/usersContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = (props) => {
+    return (
+        <div className={'app_wrapper'}>
+            <Header />
+            <Nav state={props.state}/>
+            <div className={'app_wrapper-content'}>
+                <Routes>
+                    <Route path="/profile" element = {<Profile/>}/>
+                    <Route exact path="/dialogs" element = {<DialogsContainer/>}/>
+                    <Route path="/musics" element = {<Musics />}/>
+                    <Route path="/settings" element = {<Settings />}/>
+                    <Route path="/users" element = {<UsersContainer />}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
+
+
 
 export default App;
